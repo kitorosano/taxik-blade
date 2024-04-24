494ad3eae21509b93e5c001384ec61d517ecfe34
Kitorosano <rosanoesteban2@gmail.com>
2024-04-24 20:31:39 -0300

    fix(Migration): :zap: changed foreign key on sessions table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,9 +25,9 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('users_id')->nullable()->index();
+            $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
-            $table->text('users_agent')->nullable();
+            $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
